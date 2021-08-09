1d344c8a7f9b406748d87f3340f0c7560d5da413
hertelukas <luki.hertel@gmail.com>
2021-08-09 20:26:30 +0200

    Initial setup

---
+++ src/main/java/MedievalTilesApplication.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MedievalTilesApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void startApplication(String[] args) {
+        launch(args);
+    }
+}
