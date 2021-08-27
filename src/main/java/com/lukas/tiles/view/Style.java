3bb4d4cac61e1a850d8e2efa76c1becff75b8294
hertelukas <luki.hertel@gmail.com>
2021-08-27 19:12:58 +0200

    Rendering all farmers finances with correct colors

--- src/main/java/com/lukas/tiles/view/Style.java
+++ src/main/java/com/lukas/tiles/view/Style.java
@@ -1,12 +1,15 @@
 package com.lukas.tiles.view;
 
 import javafx.geometry.Insets;
+import javafx.scene.paint.Color;
 
 public class Style {
     private final static double V_SPACING = 20;
     private final static double H_SPACING = 40;
     private final static Insets PADDING = new Insets(50);
     private final static Insets SMALL_PADDING = new Insets(20);
+    private final static String CHART_STYLE = "/fxml/stylesheets/chart.css";
+
 
     public static double getVSpacing() {
         return V_SPACING;
@@ -23,4 +26,16 @@ public class Style {
     public static Insets getSmallPadding() {
         return SMALL_PADDING;
     }
+
+    public static String getChartStyle() {
+        return CHART_STYLE;
+    }
+
+    public static String convertToWebString(Color c) {
+        int r = (int) (255 * c.getRed());
+        int g = (int) (255 * c.getGreen());
+        int b = (int) (255 * c.getBlue());
+        return String.format("#%02x%02x%02x", r, g, b);
+    }
+
 }
--- src/main/java/com/lukas/tiles/view/game/tab/FinanceView.java
+++ src/main/java/com/lukas/tiles/view/game/tab/FinanceView.java
@@ -2,6 +2,7 @@ package com.lukas.tiles.view.game.tab;
 
 import com.lukas.tiles.model.Farmer;
 import com.lukas.tiles.model.Money;
+import com.lukas.tiles.view.Style;
 import javafx.application.Platform;
 import javafx.beans.property.LongProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -12,38 +13,34 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.util.StringConverter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class FinanceView extends AbstractTabView {
 
-    private final Farmer farmer;
+    private final List<Farmer> farmers;
     private final LongProperty time;
     private final LineChart<Number, Number> moneyHistoryChart;
     private final NumberAxis timeAxis;
     private final NumberAxis moneyAxis;
-    private final XYChart.Series<Number, Number> series;
+    private final List<XYChart.Series<Number, Number>> series;
 
     // TODO: 8/27/21 show all other farmers finance
     public FinanceView(List<Farmer> farmers, LongProperty time) {
         super(new SimpleStringProperty("Finance"));
-        this.farmer = farmers.get(0);
+        this.farmers = farmers;
         this.time = time;
         this.timeAxis = new NumberAxis();
         this.moneyAxis = new NumberAxis();
-        this.series = new XYChart.Series<>();
+        this.series = new ArrayList<>();
         this.moneyHistoryChart = new LineChart<>(timeAxis, moneyAxis);
         initialize();
     }
 
     @Override
     void initialize() {
-        time.addListener((observable, oldValue, newValue) -> {
-            if (newValue.longValue() % 10 == 0) {
-                updateChart();
-            }
-        });
-        this.moneyHistoryChart.getData().add(series);
+        this.getStylesheets().add(Style.getChartStyle());
         timeAxis.setForceZeroInRange(false);
         moneyAxis.setForceZeroInRange(false);
         moneyAxis.setTickLabelFormatter(new StringConverter<>() {
@@ -57,26 +54,54 @@ public class FinanceView extends AbstractTabView {
                 return null;
             }
         });
-        series.setName(farmer.getName());
 
-        //Set the color
-        String rgb = String.format("%d, %d, %d",
-                (int) (farmer.getColor().getRed() * 255),
-                (int) (farmer.getColor().getGreen() * 255),
-                (int) (farmer.getColor().getBlue() * 255));
-        series.nodeProperty().get().setStyle("-fx-stroke: rgba(" + rgb + ", 1);");
+        //Instantiate series
+        for (Farmer value : farmers) {
+            XYChart.Series<Number, Number> temp = new XYChart.Series<>();
+            temp.setName(value.getName());
+            series.add(temp);
+        }
 
-        // TODO: 8/27/21 make the right color 
+        this.moneyHistoryChart.getData().addAll(series);
+
+        //Apply style
+        StringBuilder style = new StringBuilder();
+        for (int i = 0; i < farmers.size(); i++) {
+            style.append("symbol-color")
+                    .append(i)
+                    .append(": ")
+                    .append(Style.convertToWebString(farmers.get(i).getColor()))
+                    .append(";");
+        }
+
+        for (int i = 0; i < farmers.size(); i++) {
+            String rgb = String.format("%d, %d, %d",
+                    (int) (farmers.get(i).getColor().getRed() * 255),
+                    (int) (farmers.get(i).getColor().getGreen() * 255),
+                    (int) (farmers.get(i).getColor().getBlue() * 255));
+            series.get(i).nodeProperty().get().setStyle("-fx-stroke: rgba(" + rgb + ", 1);");
+        }
+
+        moneyHistoryChart.setStyle(style.toString());
+
         updateChart();
+
+        time.addListener((observable, oldValue, newValue) -> {
+            if (newValue.longValue() % 10 == 0) {
+                updateChart();
+            }
+        });
 
         this.getChildren().add(moneyHistoryChart);
     }
 
     private void updateChart() {
-        series.getData().removeIf(numberNumberData -> true);
+        for (int i = 0; i < series.size(); i++) {
+            series.get(i).getData().removeIf(numberNumberData -> true);
 
-        for (int i = 0; i < 12 && i < farmer.getMoneyHistory().size(); i++) {
-            series.getData().add(new XYChart.Data<>((time.get() - i * 10L), farmer.getMoneyHistory().get(farmer.getMoneyHistory().size() - (i + 1))));
+            for (int j = 0; j < 12 && j < farmers.get(i).getMoneyHistory().size(); j++) {
+                series.get(i).getData().add(new XYChart.Data<>((time.get() - j * 10L), farmers.get(i).getMoneyHistory().get(farmers.get(i).getMoneyHistory().size() - (j + 1))));
+            }
         }
     }
 }
